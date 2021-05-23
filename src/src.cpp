0f0413a4416e96cc8f1ad8c1642bbcb501b4eb64
saikamon <64338910+saikamon@users.noreply.github.com>
2021-05-23 22:24:39 +0900

    Update src.cpp

--- src/src.cpp
+++ src/src.cpp
@@ -1,25 +1,45 @@
 #include <iostream>
 #include <thread>
 
+enum STATE
+{
+    STATE_credit,
+    STATE_title,
+    STATE_opening,
+    STATE_demo,
+};
+
 int main()
 {
-repeat:
-    std::cout << "credit\n";
+    int STATE = STATE_credit;
+    while (true)
+    {
+        switch (STATE)
+        {
+            case STATE_credit:
+                std::cout << "credit\n";
+                std::this_thread::sleep_for(std::chrono::seconds(3));// 3 秒待つ
+                state_ = STATE_title;
+                break;
 
-    std::this_thread::sleep_for(std::chrono::seconds(3));// 3 秒待つ
+            case  STATE_title:
+                std::cout << "title\n";
+                std::this_thread::sleep_for(std::chrono::seconds(3));// 3 秒待つ
+                state_ = STATE_opening;
+                break;
 
-    std::cout << "title\n";
+            case STATE_opening:
+                std::cout << "opening\n";
+                std::this_thread::sleep_for(std::chrono::seconds(5));// 5 秒待つ
+                state_ = STATE_demo;
+                break;
 
-    std::this_thread::sleep_for(std::chrono::seconds(3));// 3 秒待つ
-
-    std::cout << "opening\n";
-
-    std::this_thread::sleep_for(std::chrono::seconds(5));// 5 秒待つ
-
-    std::cout << "demo\n";
-
-    std::this_thread::sleep_for(std::chrono::seconds(5));// 5 秒待つ
-    
-    goto repeat;
+            case STATE_demo:
+                std::cout << "demo\n";
+                std::this_thread::sleep_for(std::chrono::seconds(5));// 5 秒待つ
+                state_ = STATE_credit;
+                break;
+        }
+    }
 }
 
